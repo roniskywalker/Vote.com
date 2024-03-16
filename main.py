c8937f5e6a66ff373ceeba43f46b3554d16163d8
Ankan Hazra <ankanhazraeducation@gmail.com>
2024-03-16 17:57:37 +0530

    Generate Voters

--- main.py
+++ main.py
@@ -72,6 +72,33 @@ def insert_candidates(conn, cur, candidate):
         candidate['candidate_id'], candidate['candidate_name'], candidate['party_affiliation'], candidate['biography'], candidate['campaign_platform'], candidate['photo_url']))
     conn.commit()
 
+def generate_voter_data():
+    response = requests.get(BASE_URL)
+    if response.status_code == 200:
+        user_data = response.json()['results'][0]
+        return {
+            "voter_id": user_data['login']['uuid'],
+            "voter_name": f"{user_data['name']['first']} {user_data['name']['last']}",
+            "date_of_birth": user_data['dob']['date'],
+            "gender": user_data['gender'],
+            "nationality": user_data['nat'],
+            "registration_number": user_data['login']['username'],
+            "address": {
+                "street": f"{user_data['location']['street']['number']} {user_data['location']['street']['name']}",
+                "city": user_data['location']['city'],
+                "state": user_data['location']['state'],
+                "country": user_data['location']['country'],
+                "postcode": user_data['location']['postcode']
+            },
+            "email": user_data['email'],
+            "phone_number": user_data['phone'],
+            "cell_number": user_data['cell'],
+            "picture": user_data['picture']['large'],
+            "registered_age": user_data['registered']['age']
+        }
+    else:
+        return "Error fetching data"
+
 if __name__=="__main__":
     try:
         conn = psycopg2.connect("host=localhost dbname=voting user=postgres password=postgres") #start connection to postgres
@@ -91,6 +118,12 @@ if __name__=="__main__":
                 # print(candidate)
 
                 insert_candidates(conn, cur, candidate)
+
+        for i in range(1000):
+            voter = generate_voter_data()
+            # print(voter_data)
+
+            # insert_voters(conn, cur, voter)
 
     except Exception as e:
         print(e)
