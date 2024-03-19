ea160c31cf0c7b0ba8dfba167da1588490f0d3b5
Ankan Hazra <ankanhazraeducation@gmail.com>
2024-03-19 23:27:09 +0530

    Define Vote Schema

--- streaming.py
+++ streaming.py
@@ -1,4 +1,5 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, TimestampType
 
 if __name__ == "__main__":
     # Initialize SparkSession
@@ -11,3 +12,32 @@ if __name__ == "__main__":
                      "/media/ankan/AnkanHazra/Personal/Data projects/Real-Time-Voting-System/postgresql-42.7.3.jar")  # PostgreSQL driver
              .config("spark.sql.adaptive.enabled", "false")  # Disable adaptive query execution
              .getOrCreate())
+
+    vote_schema = StructType([
+        StructField("voter_id", StringType(), True),
+        StructField("candidate_id", StringType(), True),
+        StructField("voting_time", TimestampType(), True),
+        StructField("voter_name", StringType(), True),
+        StructField("party_affiliation", StringType(), True),
+        StructField("biography", StringType(), True),
+        StructField("campaign_platform", StringType(), True),
+        StructField("photo_url", StringType(), True),
+        StructField("candidate_name", StringType(), True),
+        StructField("date_of_birth", StringType(), True),
+        StructField("gender", StringType(), True),
+        StructField("nationality", StringType(), True),
+        StructField("registration_number", StringType(), True),
+        StructField("address", StructType([
+            StructField("street", StringType(), True),
+            StructField("city", StringType(), True),
+            StructField("state", StringType(), True),
+            StructField("country", StringType(), True),
+            StructField("postcode", StringType(), True)
+        ]), True),
+        StructField("email", StringType(), True),
+        StructField("phone_number", StringType(), True),
+        StructField("cell_number", StringType(), True),
+        StructField("picture", StringType(), True),
+        StructField("registered_age", IntegerType(), True),
+        StructField("vote", IntegerType(), True)
+    ])
